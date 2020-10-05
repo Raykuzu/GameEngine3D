499222d729c9268796c839d12097535aee20c138
Raykuzu <raykuzu@desktop-52ckj19.home>
2020-10-05 21:33:16 +0200

    add basic physics module

---
+++ src/modules/physics/PhysicsModule.hpp
@@ -0,0 +1,14 @@
+//
+// Created by raykuzu on 05/10/2020.
+//
+
+#pragma once
+
+#include "Module.hpp"
+
+class PhysicsModule : public Module {
+    public:
+        PhysicsModule() = default;
+        ~PhysicsModule() override = default;
+    private:
+};
