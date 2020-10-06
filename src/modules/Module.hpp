326fd4c31e1ac6ab4b0d66b4a541eaa2b3fc7d18
Raykuzu <raykuzu@desktop-52ckj19.home>
2020-10-06 20:28:53 +0200

    add componenent logic through gameObjects + run loop over modules

--- src/modules/Module.hpp
+++ src/modules/Module.hpp
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "ModuleConfiguration.hpp"
+#include "component.hh"
 
 class Module {
     public:
@@ -39,11 +40,26 @@ class Module {
             this->_moduleConfiguration = configuration;
         }
 
+        [[nodiscard]] const ModuleConfiguration &getModuleConfiguration() const {
+            return _moduleConfiguration;
+        }
+
+        void registerComponent(component_pointer component) {
+            _components.push_back(component);
+        }
+
+        void unregisterComponent(component_pointer component) {
+            _components.erase(std::remove(_components.begin(), _components.end(), component), _components.end());
+        }
+
         virtual void init() = 0;
 
         virtual void term() = 0;
 
         virtual void update() = 0;
+
+    protected:
+        std::vector<component_pointer> _components;
 
     private:
         ModuleConfiguration _moduleConfiguration;
--- src/modules/ModuleManager.hpp
+++ src/modules/ModuleManager.hpp
@@ -9,6 +9,7 @@
 #include "ArcLogger.hpp"
 #include "GlobalConfiguration.hpp"
 #include "PhysicsModule.hpp"
+#include "GameObjectManipulator.hpp"
 
 class ModuleManager {
     public:
@@ -35,13 +36,74 @@ class ModuleManager {
             std::sort(_modules.begin(), _modules.end());
         }
 
+        [[noreturn]] void run() { // temporary infinite
+            while (true) {
+                for (auto module : _modules) {
+                    module->update();
+                }
+
+            }
+        }
+
+        GameObject *registerGameObject(unsigned int componentsFlags) {
+            if (!(componentsFlags & Component::TRANSFORM) /*&& !(componentFlags & COMP)*/) {// And here
+                ArcLogger::error("GameObject need one or more components");
+                return (nullptr);
+            }
+
+            GameObject *gameObject = new GameObject(componentsFlags);
+            component_pointer componentPointer = nullptr;
+
+            for (auto compPair : compMap) {
+                if (componentsFlags & compPair.first) {
+                    componentPointer = GameObjectManipulator::createComponent(gameObject, compPair.first);//gameObject->createComponent(compPair.first);
+                    getModuleByName(nameBinaryFlagModulesBridge[compPair.first])->registerComponent(componentPointer);
+                }
+            }
+            _gameObjects.push_back(gameObject);
+            return (gameObject);
+        }
+
+        void unregisterGameObject(GameObject *gameObject) {
+
+            component_pointer componentPointer = nullptr;
+
+            for (auto compPair : compMap) {
+                if (gameObject->getFlags() & compPair.first) {
+                    componentPointer = GameObjectManipulator::deleteComponent(gameObject, compPair.first);
+                    getModuleByName(nameBinaryFlagModulesBridge[compPair.first])->unregisterComponent(componentPointer);
+                }
+            }
+            _gameObjects.erase(std::remove(_gameObjects.begin(), _gameObjects.end(), gameObject), _gameObjects.end());
+        }
+
+
     private:
+        Module *getModuleByName(std::string const &name) {
+            for (auto module : _modules) {
+                if (name == module->getModuleConfiguration().getName()) {
+                    return (module);
+                }
+            }
+            return (nullptr);
+        }
+
         static std::map<std::string, std::pair<Module *, bool>> availableModules;
+
+        static std::map<unsigned int, std::string> nameBinaryFlagModulesBridge;
 
         GlobalConfiguration _configuration;
         std::vector<Module *> _modules;
+        std::vector<GameObject *> _gameObjects;
+
+
+        friend class GameObject;
 };
 
 std::map<std::string, std::pair<Module *, bool>> ModuleManager::availableModules = {
         {"physics", {new PhysicsModule(), false}},
+};
+
+std::map<unsigned int, std::string> ModuleManager::nameBinaryFlagModulesBridge = {
+        {Component::TRANSFORM, "physics"},
 };
