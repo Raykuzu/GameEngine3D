2895ea0b85d82734e772e725223138ab38468cdf
Raykuzu <raykuzu@desktop-52ckj19.home>
2020-10-06 16:22:28 +0200

    add Core class

---
+++ src/core/Core.hpp
@@ -0,0 +1,18 @@
+//
+// Created by raykuzu on 06/10/2020.
+//
+
+#pragma once
+
+#include "ModuleManager.hpp"
+
+class Core {
+    public:
+        Core() = default;
+        ~Core() = default;
+        void run() {
+
+        }
+    private:
+        ModuleManager _moduleManager;
+};
