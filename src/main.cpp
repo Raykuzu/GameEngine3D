43442eb1052f9801312f7a4c08320b79ee9d6555
Antoine <Antoine Laget>
2020-10-12 15:40:12 +0200

    correction on main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 // Created by raykuzu on 30/09/2020.
 //
 #include <iostream>
-#include "EngineMath.hpp"
 
 int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av) {
-    std::cout << "oui" << EngineMath::add(2, 3) << std::endl;
     return (0);
 }
