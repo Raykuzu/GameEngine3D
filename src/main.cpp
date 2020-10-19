55bdfcc141be0f222d83accd2fcc276003a8b4c2
Raykuzu <raykuzu@localhost.localdomain>
2020-10-19 18:43:46 +0200

    add core in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 //
 #include <iostream>
 
+#include "Core.hpp"
+
 int main(__attribute__((unused)) int ac, __attribute__((unused)) char **av) {
+    ArcLogger::setBinaryName("GE3D");
+    ArcLogger::setOutputDir("./log");
+    ArcLogger::setLogLevel(ArcLogger::TRACE);
+    ArcLogger::setLogLevel(ArcLogger::DEBUG);
+    Core core = Core();
+
+    core.run();
     return (0);
 }
