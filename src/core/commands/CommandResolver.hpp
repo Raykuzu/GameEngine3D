a37fff57d8a67458526bc7923b93e309c5c8d280
Raykuzu <raykuzu@localhost.localdomain>
2020-10-19 17:42:04 +0200

    add commands handler

---
+++ src/core/commands/CommandResolver.hpp
@@ -0,0 +1,48 @@
+//
+// Created by raykuzu on 14/10/2020.
+//
+
+#pragma once
+
+#include <map>
+#include "Command.hpp"
+#include "StringHelper.hpp"
+
+class CommandResolver{
+    public:
+        CommandResolver() = default;
+        ~CommandResolver() = default;
+
+        static Command resolve(std::string const &line) {
+            ArcLogger::trace("CommandResolver::resolve");
+            ArcLogger::debug("Line: " + line);
+
+            Command command = Command();
+            std::vector<std::string> words = StringHelper::split(line, ' ');
+
+            if (words.empty()) {
+                ArcLogger::warn("Empty command string");
+                return (command); // implicit none command
+            }
+            if (commandsMap.count(words.at(0)) == 0) {
+                ArcLogger::warn("Unknown command: " + words.at(0));
+                return (command); // same
+            } else {
+                if (words.size() - 1 != commandsMap.at(words.at(0)).second) {
+                    ArcLogger::warn("Number of parameters mismatch for the command: " + words.at(0));
+                    return (command);
+                }
+                command.setType(commandsMap.at(words.at(0)).first);
+                for (size_t i = 1; i < words.size(); i++) {
+                    command.addValue(words.at(i));
+                }
+            }
+            return (command);
+        }
+    private:
+        static std::map<std::string, std::pair<CommandType, size_t>> commandsMap;
+};
+
+std::map<std::string, std::pair<CommandType, size_t>> CommandResolver::commandsMap = {
+        {"load", {CommandType::LOAD, 1}}
+};
