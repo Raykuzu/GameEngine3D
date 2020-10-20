cf78ab5ba8c34aa8db3dc1dddcb2508ed9d53909
Raykuzu <raykuzu@localhost.localdomain>
2020-10-20 02:24:50 +0200

    lot of things (thread for mm + print shell + misc)

--- src/game-engine/core/Core.hpp
+++ src/game-engine/core/Core.hpp
@@ -7,7 +7,9 @@
 #include "GamePlayFramework.hpp"
 #include "LibLoader.hpp"
 #include "CommandResolver.hpp"
-//#include "IGame.hpp"
+#include "IConfiguration.hpp"
+#include "AGame.hpp"
+#include "ThreadHandler.hpp"
 
 class Core {
     public:
@@ -20,18 +22,19 @@ class Core {
 
     private:
         void loadConfiguration() {
-            if (_confLoader.load("conf/conf.so") == -1) {
+            if (_confLoader.load("./libconfiguration.so") == -1) {
                 throw std::runtime_error("Can't load configuration");
             }
-            GlobalConfiguration *confPtr = ((GlobalConfiguration * (*)())_confLoader.getFunction("getConfiguration"))();
-            GlobalConfiguration conf = GlobalConfiguration(*confPtr);
+            _conf = ((IConfiguration * (*)())_confLoader.getFunction("loadConf"))();
 
-            delete confPtr;
-            _moduleManager.configure(conf);
-            ArcLogger::info(conf.getModules().at("physics").getName());
+            _moduleManager.configure(_conf->getConfiguration());
+            ArcLogger::info(_conf->getConfiguration().getModules().at("physics").getName());
         }
 
         void runShell() {
+            ArcLogger::vanilla("Core loaded .....");
+            ArcLogger::vanilla("Shell running, for information type HELP.");
+            ArcLogger::vanilla("-----------------------------------------");
             std::string line;
             Command command;
 
@@ -40,24 +43,78 @@ class Core {
                 switch (command.getType()) {
                     case NONE:
                         break;
-                    case LOAD:
-                        if (_gameLoader.somethingLoaded()) {
-                            //((IGame * (*)())_gameLoader.getFunction("loadGame"))(_game);
-                            _gameLoader.unload();
-                        }
-                        if (_gameLoader.load(command.getValues().at(0))) {
-                            continue;
-                        }
-                        // _game = ((IGame * (*)())_gameLoader.getFunction("loadGame"))();
-
+                    case HELP:
+                        printHelp();
+                        break;
+                    case LOAD_GAME:
+                        unloadGame();
+                        loadGame(command.getValues().at(0));
+                        teardownModuleManager();
+                        _game->init();
+                        configureAndRunModuleManager();
+                        break;
+                    case LOAD_CONF:
+                        unloadConf();
+                        loadConf(command.getValues().at(0));
+                        teardownModuleManager();
+                        configureAndRunModuleManager();
                         break;
                 }
             }
         }
+
+        void configureAndRunModuleManager() {
+            _moduleManager.configure(_conf->getConfiguration());
+            _moduleManagerTHandler.start(&ModuleManager::run, _moduleManager);
+        }
+
+        void teardownModuleManager() {
+            _moduleManagerTHandler.stop();
+            _moduleManager.destroy();
+        }
+
+        void loadGame(std::string const &path) {
+            if (_gameLoader.load(path)) {
+                ArcLogger::error("Can't load game at path [" + path + "]");
+                return;
+            }
+            _game = ((AGame * (*)(GamePlayFramework &gamePlayFramework))_gameLoader.getFunction("loadGame"))(_gamePlayFramework);
+        }
+
+        void unloadGame() {
+            if (_gameLoader.somethingLoaded()) {
+                _game->term();
+                ((void (*)(AGame *))_gameLoader.getFunction("unloadGame"))(_game);
+                _gameLoader.unload();
+            }
+        }
+
+        void loadConf(std::string const &path) {
+            if (_confLoader.load(path)) {
+                ArcLogger::error("Can't load conf at path [" + path + "]");
+                return;
+            }
+            _conf = ((IConfiguration * (*)())_confLoader.getFunction("loadConf"))();
+        }
+
+        void unloadConf() {
+            if (_confLoader.somethingLoaded()) {
+                ((void (*)(IConfiguration *))_confLoader.getFunction("unloadConf"))(_conf);
+                _confLoader.unload();
+            }
+        }
+
+        static void printHelp() {
+            ArcLogger::vanilla("Hello it's the help //TODO");
+        }
+
         ModuleManager _moduleManager;
+        ThreadHandler _moduleManagerTHandler;
+
         GamePlayFramework _gamePlayFramework;
 
-        //IGame *_game;
+        AGame *_game = nullptr;
+        IConfiguration *_conf = nullptr;
 
         LibLoader _confLoader;
         LibLoader _gameLoader;
